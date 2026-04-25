ae70ae686abdf101597468dc9cc8bbb88fd71004
mgiks <lidamil6785@gmail.com>
2026-04-26 01:24:43 +0600

    feat: center header and main on home page, add average and median task completion boxes

---
+++ script.js
@@ -0,0 +1,31 @@
+(async function () {
+  const data = [
+    { day: 1, tasks: 3 },
+    { day: 2, tasks: 7 },
+    { day: 3, tasks: 1 },
+    { day: 4, tasks: 8 },
+    { day: 5, tasks: 4 },
+    { day: 6, tasks: 10 },
+    { day: 7, tasks: 2 },
+  ];
+
+  new Chart(document.getElementById("task-completion-chart"), {
+    type: "line",
+    data: {
+      labels: data.map((row) => row.day),
+      datasets: [
+        {
+          label: "Tasks completed",
+          data: data.map((row) => row.tasks),
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+})();
